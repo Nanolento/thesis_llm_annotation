92d240312444795229a7f10241ec396226284d63
Jelmer Smit <jelmersmit@upcmail.nl>
2025-05-09 15:55:34 +0200

    Attempted fix for LLM ignoring specified format

--- main.py
+++ main.py
@@ -20,10 +20,12 @@ of these should be annotated on a Likert scale from 1 to 5. You will also annota
 has a story or contains a narrative or not. This is a binary class which you should annotate as yes or no.
 
 OUTPUT
-As output, provide only the annotations in the format described below.
+As output, provide only the annotations in the format described below. Only use the categories "Story", "Suspense",
+"Curiosity" and "Surprise". Do not use any other categories, only annotate these ones.
 
 FORMAT OF OUTPUT
-Please only provide your annotations in a "Element: Annotation" format, like in the below example.
+Please only provide your annotations in a "Element: Annotation" format, like in the below example. Do not put "Element:" in front
+of the annotation, just the category followed by a colon and a space and then the annotation you created.
 Story: yes
 Suspense: 3
 Curiosity: 2
@@ -53,19 +55,25 @@ def main():
         # Parse output
         output = json.loads(response.text)
         print(f"\nPassage ID = {comment['name']}\n{output['response']}")
-        for line in output["response"].split("\n"):
-            line2 = line.split(":")
-            prop = line2[0].lower()
-            val = line2[1].lstrip() # Remove leading space.
-            if prop == "story":
-                annotations[comment["name"]][prop] = val == "yes" # convert yes/no to true/false
-            else:
-                annotations[comment["name"]][prop] = int(val)
+        try:
+            for line in output["response"].split("\n"):
+                line2 = line.split(":")
+                prop = line2[0].lower()
+                val = line2[1].lstrip() # Remove leading space.
+                if prop == "story":
+                    annotations[comment["name"]][prop] = val == "yes" # convert yes/no to true/false
+                else:
+                    annotations[comment["name"]][prop] = int(val)
+        except (IndexError, ValueError):
+            print("Broken annotation, ignoring.")
+            if comment["name"] in annotations:
+                del annotations[comment["name"]]
+            continue
 
-        # Save output
-        with open("annotations.json", "w") as f:
-            print("Saving output")
-            json.dump(annotations, f)
+    # Save output
+    with open("annotations.json", "w") as f:
+        print("Saving output")
+        json.dump(annotations, f)
 
 
 if __name__ == "__main__":
